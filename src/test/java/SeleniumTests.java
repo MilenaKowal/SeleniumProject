6101518b1102a769b14596c55901a1f65182646e
MilenaKowal <jachczyk.milena@gmail.com>
2020-01-18 13:15:48 +0100

    Tescik

---
+++ src/test/java/SeleniumTests.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+
+public class SeleniumTests {
+
+    public WebDriver driver;
+
+    @Before
+    public void SetUp(){
+
+    }
+
+
+}
